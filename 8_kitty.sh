156f312f61a855f2d57e405949aaf40d56f33922
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-23 23:38:09 +0430

    Update 8_kitty.sh

--- 8_kitty.sh
+++ 8_kitty.sh
@@ -2,3 +2,6 @@
 
 mkdir $HOME/.config/kitty
 cp $HOME/archfull/configs/kitty/kitty.conf $HOME/.config/kitty
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+cd ~/.config/kitty
+ln -s ./kitty-themes/themes/Monokai.conf ~/.config/kitty/theme.conf
