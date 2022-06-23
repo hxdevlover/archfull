bb8d17c998d76df7046610b1845aaf5d9663c35b
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-23 23:10:53 +0430

    Update 5_doomEmacs.sh

--- 5_doomEmacs.sh
+++ 5_doomEmacs.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/fish
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 fish_add_path ~/.emacs.d/bin
 doom sync
+alias emacs "/usr/bin/emacsclient -c -a 'emacs'"
+funcsave emacs
 
-cp $HOME/archfull/emacsclient.desktop /usr/share/applications/
+sudo cp $HOME/archfull/emacsclient.desktop /usr/share/applications/
