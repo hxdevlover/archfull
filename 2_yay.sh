35a3dba2344779696d559bf65e46caa8c646a6c0
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-23 22:45:27 +0430

    Update 2_yay.sh

--- 2_yay.sh
+++ 2_yay.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+sudo pacman -S base-devel git curl
 mkdir $HOME/tmp
 sudo pacman -S --noconfirm --needed base-devel git
-git clone https://aur.archlinux.org/yay.git $HOME/tmp
+git clone https://aur.archlinux.org/yay.git $HOME/tmp/yay
 sudo chown -R  hx:hx $HOME/tmp/yay
 cd $HOME/tmp/yay
 makepkg -si
