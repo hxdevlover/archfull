c211454c7044b52493b8ebb6f06fc3258a01b40d
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-25 20:42:36 +0430

    Update 9_devs.sh

--- 9_devs.sh
+++ 9_devs.sh
@@ -4,7 +4,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install language servers
-sudo pacman -Sy typescript-language-server typescript rust-analyzer python3 python-pip pyright
+sudo pacman -Sy npm typescript-language-server typescript rust-analyzer python3 python-pip pyright vscode-html-languageserver vscode-css-languageserver
 
 # install pyright
 # rcommended to install anaconda3
