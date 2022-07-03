f7d75546a11d05cc0bebc6c3774d5cff5d37d756
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-07-03 19:42:30 +0430

    Update 7_fonts_fontConfigs.sh

--- 7_fonts_fontConfigs.sh
+++ 7_fonts_fontConfigs.sh
@@ -4,4 +4,4 @@ mkdir $HOME/.local/share/fonts
 mkdir $HOME/.config/fontconfig/
 
 cp -r $HOME/archfull/fonts/* $HOME/.local/share/fonts
-cp $HOME/archfull/fonts.conf $HOME/.config/fontconfig/
+cp $HOME/archfull/configs/fontconfig/fonts.conf $HOME/.config/fontconfig/
