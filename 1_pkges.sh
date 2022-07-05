9716867e5532065bf88b5406823f16e0509d0fdc
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-07-05 15:02:31 +0430

    Update 1_pkges.sh

--- 1_pkges.sh
+++ 1_pkges.sh
@@ -5,7 +5,7 @@ echo "Installing Pkgs..."
 sleep 5; sudo pacman -Sy --needed --noconfirm base-devel git xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 
 # WMs & Needed Pkgs
-sleep 5; sudo pacman -S --needed --noconfirm awesome qtile firefox nitrogen picom pcmanfm lxappearance rofi kitty volumeicon network-manager-applet python-pip htop emacs neovim polkit lxsession xdg-user-dirs
+sleep 5; sudo pacman -S --needed --noconfirm awesome qtile firefox nitrogen picom pcmanfm lxappearance rofi kitty volumeicon network-manager-applet python-pip htop emacs neovim polkit lxsession xdg-user-dirs python-psutil
 
 # Wallpapers & Themes
 sleep 5; sudo pacman -S --needed --noconfirm materia-gtk-theme papirus-icon-theme archlinux-wallpaper arc-gtk-theme
@@ -17,11 +17,9 @@ sleep 5; sudo pacman -S --needed --noconfirm alsa-utils pamixer unrar unzip p7zi
 sleep 5; sudo pacman -S --needed --noconfirm ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family ttf-fira-code ttf-fira-mono adobe-source-code-pro-fonts adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 # Other useful pkgs
-sleep 5; sudo pacman -S --needed --noconfirm bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip feh acpid jedi-language-server curl wget go ripgrep fd nodejs yarn
+sleep 5; sudo pacman -S --needed --noconfirm bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager viewnior yt-dlp xorg-xclipboard xclip feh acpid jedi-language-server curl wget go ripgrep fd nodejs
 
 # TouchPad
 sleep 5; sudo pacman -S --needed --noconfirm xorg-xinput
 
 chsh -s /usr/bin/fish
-
-pip install psutil
